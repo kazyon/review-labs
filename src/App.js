206fbb05391df248c12cacdc0f289638b74e82fb
Bacitea Dragos <bacatea@gmail.com>
2022-10-08 09:52:01 +0300

    test components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Test1 from "./components/test1";
 
 
 function App() {
-  return <div>Hello</div>;
-};
+  return (
+  <div>
+    <h1>My todos</h1>
+    <Test1 text= 'test1'/> 
+    <Test1 text= 'test2'/> 
+    <Test1 text= 'test3'/> 
+  </div>
+  );
+}
 
 export default App;
---
+++ src/components/test1.js
@@ -0,0 +1,12 @@
+function Test1(props) {
+    return (
+        <div className="card">
+            <h2>{props.text}</h2>
+                <div>
+                    <button className="btn">Press me</button>
+                </div>
+        </div>
+    );
+}
+
+export default Test1;
